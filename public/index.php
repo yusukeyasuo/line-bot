02531f380025c7f0208ae19375f812e14d746c58
Masatoshi Hiraoka <hirarin@sonicmoov.com>
2016-12-18 03:32:35 +0900

    add public/index.php

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * index.php
+ *
+ * @copyright   Copyright (c) 2016 sonicmoov Co.,Ltd.
+ * @version     $Id$
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+echo 'Hello World.';
